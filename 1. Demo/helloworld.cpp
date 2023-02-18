cc1e7f4494b322ab35119b689aa45d2b08caa101
Ritik <ritikkhulbe22@gmail.com>
2023-02-18 22:58:20 +0530

    Hello World Fixed

--- 1. Demo/helloworld.cpp
+++ 1. Demo/helloworld.cpp
@@ -2,26 +2,6 @@
 #include <iostream>
 using namespace std;
 
-class Ritik{
-    public:
-    int getint(){
-        int n;
-        cin>>n;
-        return n;
-    }
-    string getstr(){
-        string n;
-        cin>>n;
-        return n;
-    }
-    int getzero(){
-        for(int i=0;i<100;i++){
-            continue;
-        }
-        return 0;
-    }
-};
 int main(){
-    Ritik r;
-    
+    cout<<"Hello World"<<endl;
 }
