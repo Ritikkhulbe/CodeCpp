14195aac5878f0beb96d78a62bf8fed827306f95
Ritik <ritikkhulbe22@gmail.com>
2023-02-24 12:41:43 +0530

    Recursive factorial of n

---
+++ 14. Recursion/Factorial.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int fact(int n){
+    if (n == 1){
+        return 1;
+    }
+    int prev = fact(n-1);
+    return n*prev;
+}
+
+int main(){
+    int n;
+    cin >> n;
+    cout<<fact(n)<<endl;
+}
